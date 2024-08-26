169da1869802104e3002687a94ae7819032b0733
VitorGuimaraesF <vitor-guimaraes@outlook.com>
2024-08-26 20:08:25 -0300

    Mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Recuperar os componentes EditText
-            //Criar uma variavel e associar(=) a um componente de UI<EditText>
+        //Criar uma variavel e associar(=) a um componente de UI<EditText>
         // Recuperar o botar da tela
         //Colocar acao no botao
         // Recuperar o texto digitado no edt peso
@@ -25,14 +26,33 @@ class MainActivity : AppCompatActivity() {
 
 
         btnCalcular.setOnClickListener {
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
+
+
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if(pesoStr == "" || alturaStr == "") {
+            // Mostrar mensagem para o usuario
+
+                Snackbar.make(
+                    edtPeso,
+                    "Preencha todos os campos do formulário",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+            }else {
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
 
 
             println("Acao do botao " + resultado)
+            }
         }
 
     }
